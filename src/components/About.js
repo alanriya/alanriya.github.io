81bb90eb0875bf426c730a953d3dede14596206c
Alan Lee <alan.leeyungchong@gmail.com>
2023-07-31 22:51:36 +0800

    update profile

--- src/components/About.js
+++ src/components/About.js
@@ -29,13 +29,20 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:w-32 lg:h-32 md:w-24 md:h-24 w-20 h-20 rounded-full overflow-hidden">
+          <img
+            className="object-cover object-center w-full h-full"
+            alt="hero"
+            src="./profile.jpg"
+          />
+        </div>
+        {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
             alt="hero"
             src="./profile.jpg"
           />
-        </div>
+        </div> */}
       </div>
     </section>
   );
