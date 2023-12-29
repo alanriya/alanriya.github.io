1b32ea3e4fb47f11c0c7f3c5d74280a44bcee783
Alan Lee <alan.leeyungchong@gmail.com>
2023-12-29 13:40:46 +0800

    update trading platform gif

--- src/data.js
+++ src/data.js
@@ -44,7 +44,7 @@ export const projects = [
     subtitle: "C# .NET 6.0 framework",
     description:
       "This is a trading platform set up using C#. it consists of the market data injector, simulated exchange, orderbook, strategy component. The data is first read from the txt file and pass the message down using multicast UDP. The orderbook object will process the message received from the UDP channel and builds the book. The Strategy object is a simple strategy that buys when instrument price is higher than a certain price. When the condition is met, the strategy will place an buy order to the simulated exchange at the best ask price. The exchange will reply with the filled order. At the end of the trading session, the all the position in the strategy will be reversed and the pnl will be computed.",
-    image: "./searchEngine.gif",
+    image: "./tradingPlatform.gif",
     link: "https://github.com/alanriya/TradingPlatformSample",
   }
 ];
